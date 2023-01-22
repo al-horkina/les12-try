93745d620c40dacc5d6ba085c54e461eedf370b5
al-horkina <al.horkina@gmail.com>
2023-01-22 21:59:47 +0100

    dz193

--- dz19-students_lab/app.js
+++ dz19-students_lab/app.js
@@ -1,15 +1,15 @@
 
 const gruppa = [
     {student: 'Vasja', runLab: runLab(23)},
-    {student: 'Tolja', runLab: runLabsa(23)},// помилка
-    {student: 'olja', runLab: runLab(20)}
+    {student: 'Tolja', runLab: runLab(25)},// помилка
+    {student: 'olja', runLab: runLab(19)}
 
 ];
 
 function gradeLabs(x) {
     try {
         x.forEach((item) => {
-            if (!item.runLab) {
+            if (item.runLab === correctResult() ) {
                 throw new TypeError('lab.runLab is not a function');
             } else {
                 console.log(`Лабораторія студента ${item.student} працює ${item.runLab}`)
@@ -23,6 +23,10 @@ function gradeLabs(x) {
 
 function runLab(a) {
     return (a <= 20) ? 'Невдало' : 'Вдало';
+}
+
+function correctResult() {
+    return 'Невдало';
 }
 
 console.log(gradeLabs(gruppa));
